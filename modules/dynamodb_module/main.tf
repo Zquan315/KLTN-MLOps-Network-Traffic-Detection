050435565de6cf6ef75ebfe9912b74828aa4dfe1
Zquan3101 <22521190@gm.uit.edu.vn>
2025-10-25 07:59:29 +0700

    Update module dynamodb

--- modules/dynamodb_module/main.tf
+++ modules/dynamodb_module/main.tf
@@ -7,6 +7,7 @@ resource "aws_dynamodb_table" "ids_flow_logs" {
 
   # Flask sử dụng flow_id làm khóa chính
   hash_key = "flow_id"
+  range_key = "timestamp"
 
   # ----------------------------
   # Define attributes
@@ -17,8 +18,8 @@ resource "aws_dynamodb_table" "ids_flow_logs" {
   }
 
   attribute {
-    name = "time"
-    type = "S"
+    name = "timestamp"
+    type = "N"
   }
 
   attribute {
@@ -31,13 +32,18 @@ resource "aws_dynamodb_table" "ids_flow_logs" {
     type = "S"
   }
 
+  attribute {
+    name = "features_json"
+    type = "S"
+  }
+
   # ----------------------------
   # GSI 1: query theo label
   # ----------------------------
   global_secondary_index {
     name            = "gsi_label"
     hash_key        = "label"
-    range_key       = "time"
+    range_key       = "timestamp"
     projection_type = "ALL"
   }
 
@@ -47,7 +53,7 @@ resource "aws_dynamodb_table" "ids_flow_logs" {
   global_secondary_index {
     name            = "gsi_content"
     hash_key        = "content"
-    range_key       = "time"
+    range_key       = "timestamp"
     projection_type = "ALL"
   }
 
@@ -64,10 +70,24 @@ locals {
   sample_logs = [
     {
       flow_id = "test-001",
-      time    = "2025-10-24 16:00:00",
-      content = "Src: 172.16.0.5 → 192.168.50.1 (6), Flow simulated",
-      label   = "BENIGN",
+      timestamp    = "1736467200000",
+      content = "Src: 172.16.1.5 → 192.168.60.1 (6), Flow simulated",
+      label   = "benign",
       features_json = "{\"Flow Duration\": 1.0, \"Total Fwd Packets\": 2}"
+    },
+    {
+      flow_id = "test-002",
+      timestamp    = "1736464260000",
+      content = "Src: 172.16.0.5 → 192.168.50.1 (6), Flow simulated",
+      label   = "attack",
+      features_json = "{\"Flow Duration\": 2.0, \"Total Fwd Packets\": 5}"
+    },
+    {
+      flow_id = "test-003",
+      timestamp    = "1736467320000",
+      content = "Src: 172.17.10.5 → 192.168.100.1 (6), Flow simulated",
+      label   = "benign",
+      features_json = "{\"Flow Duration\": 1.5, \"Total Fwd Packets\": 3}"
     }
   ]
 }
@@ -76,12 +96,13 @@ resource "aws_dynamodb_table_item" "seed" {
   for_each   = { for s in local.sample_logs : s.flow_id => s }
   table_name = aws_dynamodb_table.ids_flow_logs.name
   hash_key   = "flow_id"
+  range_key  = "timestamp"
 
   item = jsonencode({
-    flow_id       = { S = each.value.flow_id }
-    time          = { S = each.value.time }
-    content       = { S = each.value.content }
-    label         = { S = each.value.label }
-    features_json = { S = each.value.features_json }
+    flow_id           = { S = each.value.flow_id }
+    timestamp         = { S = each.value.timestamp }
+    content           = { S = each.value.content }
+    label             = { S = each.value.label }
+    features_json     = { S = each.value.features_json }
   })
 }
